dcd101bda70cbe0e8905072ddfad9dadc9ce74cc
Matthew Kapitan <mkapitan@my.wctc.edu>
2020-02-03 20:33:44 -0600

    Fixed typo with "Week" instead of "2week"

--- SleepData/Program.cs
+++ SleepData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SleepData
 {
@@ -15,7 +16,7 @@ namespace SleepData
             string resp = Console.ReadLine();
 
             // specify path for data file
-            string file = "/users/jgrissom/downloads/data.txt";
+            string file = AppDomain.CurrentDomain.BaseDirectory + "data.txt";
 
             if (resp == "1")
             {
@@ -28,8 +29,10 @@ namespace SleepData
 
                 // determine start and end date
                 DateTime today = DateTime.Now;
+
                 // we want full weeks sunday - saturday
                 DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+
                 // subtract # of weeks from endDate to get startDate
                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
 
@@ -58,8 +61,39 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
+                Console.Clear();
+                string dayFormat = "    {0,-3}{1,-3}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-4}";
 
+                Console.WriteLine("\n");
+                StreamReader sr = new StreamReader(file);
+                while (!sr.EndOfStream)
+                {
+
+                    string entry = sr.ReadLine();
+                    string[] week = entry.Split(',');
+                    string[] day = week[1].Split('|');
+
+
+                    for (int i = 0; i < week.Length - 1; i++)
+                    {
+                        DateTime date = Convert.ToDateTime(week[0]);
+                        Console.WriteLine("  Week of  {0:MMM, dd, yyyy}",date);
+
+                        int total = 0;
+                        for (int t = 0; t < day.Length; t++)
+                        {
+                            total += Int32.Parse(day[t]);
+                        }
+                        int avg = total / 7;
+
+                        Console.WriteLine(dayFormat, "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Tot","Avg");
+                        Console.WriteLine(dayFormat, "--", "--", "--", "--", "--", "--", "--", "---","---");
+                        Console.WriteLine(dayFormat, day[0], day[1], day[2], day[3], day[4], day[5], day[6], total, avg);
+                        Console.WriteLine();
+                    }
+                }
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
             }
         }
     }
